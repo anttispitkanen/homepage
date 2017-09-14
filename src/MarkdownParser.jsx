c1030c758619f0588fc48737bd7a86daa8004ff6
Antti <anttis.pitkanen@gmail.com>
2017-09-14 22:27:13 +0300

    Move BASE_URL definition to a constants file.

--- src/MarkdownParser.jsx
+++ src/MarkdownParser.jsx
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import marked from 'marked';
 import PropTypes from 'prop-types';
+import { BASE_URL } from './constants';
 
 class MarkdownParser extends Component {
     constructor(props) {
         super(props);
-        this.baseUrl = 'https://raw.githubusercontent.com/anttispitkanen/homepage-content/master/';
         this.parseMarkdown = this.parseMarkdown.bind(this);
         this.state = {
             fetchedMarkdown: '',
@@ -15,7 +15,7 @@ class MarkdownParser extends Component {
 
     async componentDidMount() {
         try {
-            const response = await fetch(this.baseUrl + this.props.url);
+            const response = await fetch(BASE_URL + this.props.url);
             const text = await response.text();
             this.setState({ fetchedMarkdown: text });
         } catch (e) {
@@ -48,6 +48,7 @@ class MarkdownParser extends Component {
 
 MarkdownParser.propTypes = {
     classNameProp: PropTypes.string.isRequired,
+    url: PropTypes.string.isRequired
 };
 
 export default MarkdownParser;
---
+++ src/constants.js
@@ -0,0 +1,2 @@
+/* Constant values used through the app */
+export const BASE_URL = 'https://raw.githubusercontent.com/anttispitkanen/homepage-content/master/';
